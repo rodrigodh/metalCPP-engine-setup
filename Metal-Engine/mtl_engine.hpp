77ec45f90fe689496658744195c58dc21ae634ed
rodrigodh <rodrigoschieck.pro@gmail.com>
2024-05-26 11:35:10 -0300

    feat: Rendering a triangle.

--- Metal-Engine/mtl_engine.hpp
+++ Metal-Engine/mtl_engine.hpp
@@ -18,6 +18,9 @@
 #include <QuartzCore/CAMetalLayer.h>
 #include <QuartzCore/QuartzCore.hpp>
 
+#include <simd/simd.h>
+#include <iostream>
+
 
 class MTLEngine {
 public:
@@ -28,9 +31,25 @@ public:
 private:
     void initDevice();
     void initWindow();
+    
+    void createTriangle();
+    void createDefaultLibrary();
+    void createCommandQueue();
+    void createRenderPipeline();
+
+    void encodeRenderCommand(MTL::RenderCommandEncoder* renderEncoder);
+    void sendRenderCommand();
+    void draw();
 
     MTL::Device* metalDevice;
     GLFWwindow* glfwWindow;
     NSWindow* metalWindow;
     CAMetalLayer* metalLayer;
+    CA::MetalDrawable* metalDrawable;
+
+    MTL::Library* metalDefaultLibrary;
+    MTL::CommandQueue* metalCommandQueue;
+    MTL::CommandBuffer* metalCommandBuffer;
+    MTL::RenderPipelineState* metalRenderPSO;
+    MTL::Buffer* triangleVertexBuffer;
 };
--- Metal-Engine/mtl_engine.mm
+++ Metal-Engine/mtl_engine.mm
@@ -11,10 +11,19 @@
 void MTLEngine::init() {
     initDevice();
     initWindow();
+    
+    createTriangle();
+    createDefaultLibrary();
+    createCommandQueue();
+    createRenderPipeline();
 }
 
 void MTLEngine::run() {
     while (!glfwWindowShouldClose(glfwWindow)) {
+        @autoreleasepool {
+            metalDrawable = (__bridge CA::MetalDrawable*)[metalLayer nextDrawable];
+            draw();
+        }
         glfwPollEvents();
     }
 }
@@ -32,20 +41,105 @@ void MTLEngine::initWindow() {
     glfwInit();
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); // We use this function to avoid starting the window with opengl graphics context.
     
-    int width = 800;
-    int height = 600;
-    const char *windowName = "Digout";
+    int windowWidth = 800;
+    int windowHeight = 600;
+    const char *windowName = "Digin";
 
-    glfwWindow = glfwCreateWindow(width, height, windowName, NULL, NULL);
+    glfwWindow = glfwCreateWindow(windowWidth, windowHeight, windowName, NULL, NULL);
     if (!glfwWindow) {
         glfwTerminate();
         exit(EXIT_FAILURE);
     }
+    
+    int width, height;
+    glfwGetFramebufferSize(glfwWindow, &width, &height);
 
     metalWindow = glfwGetCocoaWindow(glfwWindow);
     metalLayer = [CAMetalLayer layer];
     metalLayer.device = (__bridge id<MTLDevice>)metalDevice;
     metalLayer.pixelFormat = MTLPixelFormatBGRA8Unorm;
+    metalLayer.drawableSize = CGSizeMake(width, height);
     metalWindow.contentView.layer = metalLayer;
     metalWindow.contentView.wantsLayer = YES;
+}
+
+// Define a triangle vertices and saves into the gpu memory buffer.
+// MMTL::ResourceStorageModeShared means that GPU and CPU are able to manage with that memory.
+
+void MTLEngine::createTriangle() {
+    simd::float3 triangleVertices[] = {
+        {-0.5f, -0.5f, 0.0f},
+        { 0.5f, -0.5f, 0.0f},
+        { 0.0f,  0.5f, 0.0f}
+    };
+
+    triangleVertexBuffer = metalDevice->newBuffer(&triangleVertices,
+                                                  sizeof(triangleVertices),
+                                                  MTL::ResourceStorageModeShared);
+}
+
+void MTLEngine::createDefaultLibrary() {
+    metalDefaultLibrary = metalDevice->newDefaultLibrary();
+    if(!metalDefaultLibrary){
+        std::cerr << "Failed to load default library.";
+        std::exit(-1);
+    }
+}
+
+void MTLEngine::createCommandQueue() {
+    metalCommandQueue = metalDevice->newCommandQueue();
+}
+
+void MTLEngine::createRenderPipeline() {
+    MTL::Function* vertexShader = metalDefaultLibrary->newFunction(NS::String::string("vertexShader", NS::ASCIIStringEncoding));
+    assert(vertexShader);
+    MTL::Function* fragmentShader = metalDefaultLibrary->newFunction(NS::String::string("fragmentShader", NS::ASCIIStringEncoding));
+    assert(fragmentShader);
+
+    MTL::RenderPipelineDescriptor* renderPipelineDescriptor = MTL::RenderPipelineDescriptor::alloc()->init();
+    renderPipelineDescriptor->setLabel(NS::String::string("Triangle Rendering Pipeline", NS::ASCIIStringEncoding));
+    renderPipelineDescriptor->setVertexFunction(vertexShader);
+    renderPipelineDescriptor->setFragmentFunction(fragmentShader);
+    assert(renderPipelineDescriptor);
+    MTL::PixelFormat pixelFormat = (MTL::PixelFormat)metalLayer.pixelFormat;
+    renderPipelineDescriptor->colorAttachments()->object(0)->setPixelFormat(pixelFormat);
+
+    NS::Error* error;
+    metalRenderPSO = metalDevice->newRenderPipelineState(renderPipelineDescriptor, &error);
+
+    renderPipelineDescriptor->release();
+}
+
+void MTLEngine::draw() {
+    sendRenderCommand();
+}
+
+void MTLEngine::sendRenderCommand() {
+    metalCommandBuffer = metalCommandQueue->commandBuffer();
+
+    MTL::RenderPassDescriptor* renderPassDescriptor = MTL::RenderPassDescriptor::alloc()->init();
+    MTL::RenderPassColorAttachmentDescriptor* cd = renderPassDescriptor->colorAttachments()->object(0);
+    cd->setTexture(metalDrawable->texture());
+    cd->setLoadAction(MTL::LoadActionClear);
+    cd->setClearColor(MTL::ClearColor(41.0f/255.0f, 42.0f/255.0f, 48.0f/255.0f, 1.0));
+    cd->setStoreAction(MTL::StoreActionStore);
+
+    MTL::RenderCommandEncoder* renderCommandEncoder = metalCommandBuffer->renderCommandEncoder(renderPassDescriptor);
+    encodeRenderCommand(renderCommandEncoder);
+    renderCommandEncoder->endEncoding();
+
+    metalCommandBuffer->presentDrawable(metalDrawable);
+    metalCommandBuffer->commit();
+    metalCommandBuffer->waitUntilCompleted();
+
+    renderPassDescriptor->release();
+}
+
+void MTLEngine::encodeRenderCommand(MTL::RenderCommandEncoder* renderCommandEncoder) {
+    renderCommandEncoder->setRenderPipelineState(metalRenderPSO);
+    renderCommandEncoder->setVertexBuffer(triangleVertexBuffer, 0, 0);
+    MTL::PrimitiveType typeTriangle = MTL::PrimitiveTypeTriangle;
+    NS::UInteger vertexStart = 0;
+    NS::UInteger vertexCount = 3;
+    renderCommandEncoder->drawPrimitives(typeTriangle, vertexStart, vertexCount);
 }
